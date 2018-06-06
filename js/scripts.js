8640ae497e62c6b375b403ce3e38072cb4dd5bcd
Andrea Goodson <andreag@pm.me>
2018-06-06 22:41:41 +0100

    Restructuring

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+const inputEl = document.getElementById('input');
+const outputEl = document.getElementById('output');
+
+function ioText(x) {
+    let inputText = input.value;
+    if (inputText != '') {
+        if (x == 'clean') {
+            inputText = inputText.trim();
+            inputText = inputText.replace(/(\r\n|\n|\r)/gm, ' ');
+            inputText = inputText.replace(/\s\s+/g, ' ');
+            outputEl.value = inputText;
+        } else if (x == 'encode') {
+            inputText = encodeURIComponent(inputText);
+            outputEl.value = inputText;
+        } else if (x == 'decode') {
+            inputText = decodeURIComponent(inputText);
+            outputEl.value = inputText;
+        } else if (x == 'swap') {
+            inputText = inputText.replace(/'/g, '---');
+            inputText = inputText.replace(/"/g, '\'');
+            inputText = inputText.replace(/---/g, '"');
+            outputEl.value = inputText;
+        } else {
+            outputEl.value = 'The input field is empty.';
+        }
+    }
+}
